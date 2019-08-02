263b371795a1b7459e3a1c71c78cfd359cd084f2
aghua <alex_hua@sfu.ca>
2019-08-02 14:59:47 -0700

    Upload 02-filter_d.py This filters out all defensemen from the CSV dataset First parameter = name of csv to be worked on Second parameter = name of csv to be output

---
+++ 02-filter_d.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import os.path
+import os
+import sys
+
+def main():
+    
+    df = pd.read_csv(sys.argv[1])
+    df = df[~(df['primaryPosition'] == 'D')]
+    print('Finished')
+    df.to_csv(path_or_buf=str(sys.argv[2]) , index=False)
+
+if __name__ == '__main__':
+    main()
