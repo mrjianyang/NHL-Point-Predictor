9c0b44207ce7634474c2198d5531235c4679bdd4
aghua <alex_hua@sfu.ca>
2019-08-02 17:14:50 -0700

    Update 02-filter_d.py

--- 02-filter_d.py
+++ 02-filter_d.py
@@ -7,6 +7,8 @@ import sys
 def main():
     
     df = pd.read_csv(sys.argv[1])
+    
+    df = df.sort_values(['player_id', 'season', 'date_time_GMT'])
     df = df[~(df['primaryPosition'] == 'D')]
     print('Finished')
     df.to_csv(path_or_buf=str(sys.argv[2]) , index=False)
